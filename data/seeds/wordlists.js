8df8f106262be3dd7f24451b145c8152f7bc03a7
Michael Harms <michael.harms6010@gmail.com>
2020-03-20 20:34:20 -0500

    fixes seed data, adds cleaner seed

--- data/seeds/wordlists.js
+++ data/seeds/wordlists.js
@@ -6,8 +6,8 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('wordlists').insert([
         {user_id: 1, wordlist: 'the,quick,brown,fox,jumped,over,the,lazy,dog,furthermore,both,were,good,boys'},
-        {user_id: 1, wordlist: 'javascript,html,sass,nodejs,nodemon,express,reactjs,contextapi,jsonwebtoken'},
-        {user_id: 2, wordlist: 'zach,michael,emily,kristi,brian,shya,webpt11,dreamteam'}
+        {user_id: 2, wordlist: 'javascript,html,sass,nodejs,nodemon,express,reactjs,contextapi,jsonwebtoken'},
+        {user_id: 3, wordlist: 'zach,michael,emily,kristi,brian,shya,webpt11,dreamteam'}
       ]);
     });
 };
