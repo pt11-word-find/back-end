46cf1f5bbcb05fede43c6cc95ff86c8426a9a7d3
Michael Harms <michael.harms6010@gmail.com>
2020-03-24 12:19:13 -0500

    updates database structure - adds table to wordlist

--- data/migrations/20200320182120_create-wordlist.js
+++ data/migrations/20200320182120_create-wordlist.js
@@ -1,17 +1,20 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('wordlists', boards => {
-    boards.increments();
-    boards.string('wordlist', 2048)
+  return knex.schema.createTable('wordlists', wordlist => {
+    wordlist.increments();
+    
+    wordlist.string('wordlist', 2048)
       .notNullable()
-    boards.integer('user_id')
+    wordlist.integer('user_id')
       .unsigned()
       .notNullable()
       .references('id')
       .inTable('users')
       .onDelete('CASCADE')
       .onUpdate('CASCADE');
+    wordlist.string("title", 256).notNullable();
     })
+   
 };
 
 exports.down = function(knex) {
--- data/seeds/wordlists.js
+++ data/seeds/wordlists.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('wordlists').insert([
-        {user_id: 1, wordlist: 'the,quick,brown,fox,jumped,over,the,lazy,dog,furthermore,both,were,good,boys'},
-        {user_id: 2, wordlist: 'javascript,html,sass,nodejs,nodemon,express,reactjs,contextapi,jsonwebtoken'},
-        {user_id: 3, wordlist: 'zach,michael,emily,kristi,brian,shya,webpt11,dreamteam'}
+        {user_id: 1, title: "Some Words", wordlist: 'the,quick,brown,fox,jumped,over,the,lazy,dog,furthermore,both,were,good,boys'},
+        {user_id: 2, title: "Tech Tools", wordlist: 'javascript,html,sass,nodejs,nodemon,express,reactjs,contextapi,jsonwebtoken'},
+        {user_id: 3, title: "Names", wordlist: 'brian,michael,emily,kristi,shya,catherine,webpt11,dreamteam'}
       ]);
     });
 };
